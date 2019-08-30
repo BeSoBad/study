6f048f5f75e2c71a1f26df49f6189948d673d582
Kriegert <artem.sizonov@mail.ru>
2019-08-30 21:02:21 +0300

    Update code.cpp

--- computer graphic/lab2/code.cpp
+++ computer graphic/lab2/code.cpp
@@ -1,16 +1,10 @@
 from matplotlib import pyplot as plt
-import pylab
-from pylab import *
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 fig.canvas.set_window_title('sizonov_aa')
-
-"""v = numpy.array([[-3, 0, 0], [0, -3, 0], [-2, 0, 0.3], \
-              [0, -2, 0.3], [3, 0, 0], [0, 3, 0], \
-              [0, 0, 3], [2, 0, 0.3]])"""
 
 v = numpy.array([[-3, 0, 0], [0, -3, 0], [3, 0, 0], \
               [0, 3, 0], [0, 0, -3], [0, 0, 3]])
@@ -22,7 +16,7 @@ verts = [[v[0], v[3], v[4]] , [v[0], v[3], v[5]],\
 		[v[0], v[1], v[4]], [v[0], v[1], v[5]], \
 		[v[2], v[3], v[4]], [v[2], v[3], v[5]]]
 
-#plt.tick_params(axis='x', which='both', bottom='off',  top='off',labelbottom='off')
+plt.tick_params(axis='x', which='both', bottom='off',  top='off',labelbottom='off')
 
 ax.add_collection(Poly3DCollection(verts, \
     facecolors=(1, 1, 1), linewidths=1, edgecolors='black', alpha=0))
@@ -30,8 +24,6 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 ax.grid(False)
-
-# Hide axes ticks
 
 plt.axis('scaled')
 plt.show()
